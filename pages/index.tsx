b8a3a8b4382889b949bfcdefaae9af124d2ae0c4
minojiro <minojiro@users.noreply.github.com>
2021-11-23 22:56:21 +0900

    added formatted line break text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
+import { useBudouX } from '../hooks/useBudouX'
 
 const Home: NextPage = () => {
+  const { parse } = useBudouX()
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
           あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。
+        </h1>
+        <h1 className={styles.title}>
+          {parse('あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。')}
         </h1>
       </main>
     </div>
